8489c1ba5e1c8d439843f663d705e2afddd7df0f
Ritika Bansal <ritikabansal032@gmail.com>
2023-12-04 21:10:54 +0530

    test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
-
+#test
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
